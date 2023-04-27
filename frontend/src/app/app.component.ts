b251546ffade857acaa49bd24361de08c6033f33
KncskBenedek <koncsikbenedek@gmail.com>
2023-04-27 10:28:51 +0200

    GETek kimentik az érkező adatokat

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,52 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { API, DataStorageService } from './shared/data-storage.service';
 import { Kategoria, Recept } from './shared/interfaces';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   receptek: Recept[] = [];
-  kategoriak: Kategoria[] = []
+  kategoriak: Kategoria[] = [];
   constructor(private dataStorageService: DataStorageService) {}
-  
-  
+
   ngOnInit(): void {
-    this.dataStorageService.get(API.RECEPT.GET)
-    .pipe(
-      tap({
-        next: (data)=>{
-          console.log(data);
-          
-        },
-        error: (error)=>{
-          console.error(error);
-          
-        }
-      })
+    this.dataStorageService
+      .get(API.RECEPT.GET)
+
+      .pipe(
+        map((data) => {
+          const newData: Recept[] = data.map((row_data) => {
+            return {
+              id: row_data.id,
+              nev: row_data.nev,
+              katId: row_data.katId,
+              kat_nev: row_data.kat.nev,
+              kepURL: row_data.kepEleresiUt,
+              leiras: row_data.leiras,
+            };
+          });
+          return [...newData];
+        }),
+        tap({
+          next: (data) => {
+            this.receptek = data;
+            console.log(this.receptek);
+          },
+          error: (error) => {
+            console.error(error);
+          },
+        })
       )
-    .subscribe();
+      .subscribe();
 
-    this.dataStorageService.get(API.KATEGORIA.GET)
-    .pipe(
-      tap({
-        next: (data)=>{
-          console.log(data);
-          
-        },
-        error: (error)=>{
-          console.error(error);
-          
-        }
-      })
-    )
-    .subscribe()
-    ;
+    this.dataStorageService
+      .get(API.KATEGORIA.GET)
+      .pipe(
+        tap({
+          next: (data: Recept[]) => {
+           this.kategoriak = data;
+           console.log(this.kategoriak);
+          },
+          error: (error) => {
+            console.error(error);
+          },
+        })
+      )
+      .subscribe();
   }
-
-
 }
